e2e1c3410fe40d0254ab5206ff9dfd8094e410d5
Julia Roldi <julialcantara.ra@gmail.com>
2024-03-30 19:29:27 -0300

    search

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     experimental: {
         swcPlugins: [['fluentui-next-appdir-directive', { paths: ['@griffel', '@fluentui'] }]],
+        missingSuspenseWithCSRBailout: false,
       },
     images:{
       domains:['ohxesicgszkzclerpknr.supabase.co']
